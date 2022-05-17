095b867278b2916d010a3a034b9f0d09fc4fb130
shamel jij <shamel.jij@gmail.com>
2022-05-17 10:45:08 +0200

    added form and paragraph and table

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 function ausrechnen(){
-    
+    let farblos1 = document.getElementById("farblos1").value;
+    let kaufenA = Number(document.getElementById("kaufenA").value);
+    let kaufenb = Number(document.getElementById("kaufenB").value);
+    let dFA = document.getElementById("dFA").value * farblos1;
+    let dFB = document.getElementById("dFB").value * farblos1;
+
+    let gesamtA = kaufenA + dFA;
+    let gesamtB = kaufenb + dFB;
+
+    document.getElementById("gesamtA").value = gesamtA;
+    document.getElementById("gesamtB").value = gesamtB;
+
 }
