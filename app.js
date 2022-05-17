4ecb34586d0b26b14fb4c36db03992275ac9b0c9
shamel jij <shamel.jij@gmail.com>
2022-05-17 14:45:50 +0200

    bis morgen

--- app.js
+++ app.js
@@ -15,6 +15,22 @@
     let consumptionB = document.getElementById("consumptionB").value * energyCost;
 
 function ausrechnen(){
+    document.getElementById("sliderPrintGray").innerHTML = document.getElementById("sliderInputGray").value; 
+    document.getElementById("sliderPrintColor").innerHTML = document.getElementById("sliderInputColor").value;
+
+    let colorPageInput = document.getElementById("sliderInputColor").value;
+    let grayPageInput = document.getElementById("sliderInputGray").value;
+    let priceA = Number(document.getElementById("priceA").value);
+    let priceB = Number(document.getElementById("priceB").value);
+    let pageGrayA = document.getElementById("pageGrayA").value * grayPageInput;
+    let pageGrayB = document.getElementById("pageGrayB").value * grayPageInput;
+    let pageColorA = document.getElementById("pageColorA").value * colorPageInput;
+    let pageColorB = document.getElementById("pageColorB").value * colorPageInput;
+    let maintenanceA = Number(document.getElementById("maintenanceA").value);
+    let maintenanceB = Number(document.getElementById("maintenanceB").value);
+    let energyCost = document.getElementById("energyCost").value;
+    let consumptionA = document.getElementById("consumptionA").value * energyCost;
+    let consumptionB = document.getElementById("consumptionB").value * energyCost;
 
     let totalA = priceA + pageGrayA + pageColorA + maintenanceA + consumptionA;
     let totalB = priceB + pageGrayB + pageColorB + maintenanceB + consumptionB;
@@ -24,12 +40,15 @@ function ausrechnen(){
     if(totalA > totalB)
     {
             document.getElementById("result").innerHTML = "B";
+            document.getElementById("deference").innerHTML = totalA - totalB;
             //document.getElementById("vA").style.color = "red";
         }else if(totalA < totalB){
             document.getElementById("result").innerHTML = "A";
+            document.getElementById("reference").innerHTML = totalB - totalA;
             //document.getElementById("vA").style.color = "green";
         }else{
             document.getElementById("result").innerHTML = "Gleichstand";
+            document.getElementById("deference").innerHTML = "Null";
     }
 }
 
@@ -38,6 +57,8 @@ function ausrechnen(){
 function showSlider(){
     document.getElementById("sliderPrintGray").innerHTML = document.getElementById("sliderInputGray").value; 
     document.getElementById("sliderPrintColor").innerHTML = document.getElementById("sliderInputColor").value; 
+    let totalA = priceA + pageGrayA + pageColorA + maintenanceA + consumptionA;
+    let totalB = priceB + pageGrayB + pageColorB + maintenanceB + consumptionB;
     //document.getElementById("farblos1").value = document.getElementById("farblosTest").value;
 }
 
